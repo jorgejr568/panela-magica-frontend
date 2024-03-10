cef2678c53feff8d0b75c0ce86fdbfdd3ca32c8a
Jorge Junior <jorge.junior.568@live.com>
2024-03-10 08:19:28 -0300

    refactor: ModalDelecao separado

---
+++ src/components/ModalDelecao.tsx
@@ -0,0 +1,78 @@
+import { Receita, ReceitasAPI } from "@/API/receita";
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+} from "@/components/ui/alert-dialog";
+import { useState } from "react";
+import { toast } from "sonner";
+
+type ModalDelecaoProps = {
+  receita: Receita;
+  isOpen: boolean;
+  onOpenChange: (open: boolean) => void;
+  afterDelete?: (receita: Receita) => void;
+};
+
+export const ModalDelecao = (props: ModalDelecaoProps) => {
+  const [isDeleting, setIsDeleting] = useState(false);
+  const handleExcluir =
+    (receita: Receita) => async (e: React.MouseEvent<HTMLButtonElement>) => {
+      setIsDeleting(true);
+
+      try {
+        await ReceitasAPI.deletar(receita.id);
+        toast.success("Receita excluída com sucesso");
+
+        props.afterDelete && props.afterDelete(receita);
+      } catch (e) {
+        toast.error("Erro ao excluir a receita");
+      } finally {
+        setIsDeleting(false);
+      }
+    };
+
+  return (
+    <AlertDialog open={props.isOpen} onOpenChange={props.onOpenChange}>
+      <AlertDialogContent className="max-w-18rem lg:max-w-[40rem]">
+        <AlertDialogHeader>
+          Deseja realmente excluir a receita?
+        </AlertDialogHeader>
+        <AlertDialogDescription>
+          <p>
+            Esta ação é irreversível e excluirá permanentemente a receita do
+            Panela Mágica.
+          </p>
+        </AlertDialogDescription>
+        <AlertDialogFooter>
+          <AlertDialogCancel>Cancelar</AlertDialogCancel>
+
+          <AlertDialogAction
+            disabled={isDeleting}
+            onClick={handleExcluir(props.receita)}
+          >
+            {isDeleting ? "Excluindo..." : "Excluir"}
+          </AlertDialogAction>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
+  );
+};
+
+export const useModalDelecao = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const openModal = () => setIsOpen(true);
+  const closeModal = () => setIsOpen(false);
+
+  return {
+    isOpen,
+    openModal,
+    closeModal,
+    onOpenChange: setIsOpen,
+  };
+};
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,63 +1,49 @@
-import {Inter} from "next/font/google";
-import {Footer, Main, Navbar} from "@/components";
-import {Receita, ReceitasAPI} from "@/API/receita";
-import {GetServerSidePropsResult} from "next";
+import { Inter } from "next/font/google";
+import { Footer, Main, Navbar } from "@/components";
+import { Receita, ReceitasAPI } from "@/API/receita";
+import { GetServerSidePropsResult } from "next";
 import Image from "next/image";
-import {humanReadableInterval} from "@/utils/date-utils";
+import { humanReadableInterval } from "@/utils/date-utils";
 import Link from "next/link";
-import {Card, CardContent, CardFooter, CardHeader} from "@/components/ui/card";
-import Head from "next/head";
-import {Button} from "@/components/ui/button";
 import {
-  AlertDialog,
-  AlertDialogAction,
-  AlertDialogCancel,
-  AlertDialogContent,
-  AlertDialogDescription,
-  AlertDialogFooter,
-  AlertDialogHeader,
-  AlertDialogTrigger
-} from "@/components/ui/alert-dialog";
-import {TrashIcon} from "@heroicons/react/24/outline";
-import {useState} from "react";
-import {toast} from "sonner";
-import {Toaster} from "@/components/ui/sonner";
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+} from "@/components/ui/card";
+import Head from "next/head";
+import { Button } from "@/components/ui/button";
+import { TrashIcon } from "@heroicons/react/24/outline";
+import { useState } from "react";
+import { toast } from "sonner";
+import { Toaster } from "@/components/ui/sonner";
+import { ModalDelecao, useModalDelecao } from "@/components/ModalDelecao";
 
-const inter = Inter({subsets: ["latin"]});
+const inter = Inter({ subsets: ["latin"] });
 
-type HomeProps = { receitas: Receita[] }
+type HomeProps = { receitas: Receita[] };
 
-export default function Home({receitas: initialReceitas}: HomeProps) {
-  const [receitas, setReceitas] = useState(initialReceitas)
-  const [isDeleting, setIsDeleting] = useState(false)
-  const handleExcluir = (receita: Receita) => async (e: React.MouseEvent<HTMLButtonElement>) => {
-    setIsDeleting(true)
+export default function Home({ receitas: initialReceitas }: HomeProps) {
+  const [receitas, setReceitas] = useState(initialReceitas);
+  const modalDelecao = useModalDelecao();
+  const afterExcluir = (receita: Receita) => {
+    setReceitas(receitas.filter((r) => r.id !== receita.id));
+  };
 
-    try {
-      await ReceitasAPI.deletar(receita.id)
-      setReceitas(receitas.filter((r) => r.id !== receita.id))
-      toast.success('Receita excluída com sucesso')
-    } catch (e) {
-      toast.error('Erro ao excluir a receita')
-    } finally {
-      setIsDeleting(false)
-    }
-  }
   return (
     <>
       <Head>
         <title>Panela Mágica</title>
       </Head>
       <div className="flex flex-col min-h-screen">
-        <Navbar/>
+        <Navbar />
 
         <Main>
-          <h1
-            className="text-3xl font-bold tracking-tight sm:text-5xl md:tracking-tighter/[-.5] lg:text-6xl lg:tracking-normal">
+          <h1 className="text-3xl font-bold tracking-tight sm:text-5xl md:tracking-tighter/[-.5] lg:text-6xl lg:tracking-normal">
             As melhores receitas para você
           </h1>
 
-          <section className='grid grid-cols-1 gap-6 mt-6 md:grid-cols-2 lg:grid-cols-3'>
+          <section className="grid grid-cols-1 gap-6 mt-6 md:grid-cols-2 lg:grid-cols-3">
             {receitas.map((receita) => (
               <Card key={receita.id}>
                 <CardHeader className="p-0 overflow-hidden">
@@ -67,61 +53,54 @@ export default function Home({receitas: initialReceitas}: HomeProps) {
                     height="225"
                     alt="Roasted Chicken"
                     className="aspect object-cover w-full"
-                    style={{aspectRatio: "400/225", objectFit: "cover"}}
+                    style={{ aspectRatio: "400/225", objectFit: "cover" }}
                   />
                 </CardHeader>
                 <CardContent>
                   <div className="grid gap-2 py-2 px-4">
-                    <h2 className="text-2xl font-semibold leading-none">{receita.nome}</h2>
+                    <h2 className="text-2xl font-semibold leading-none">
+                      {receita.nome}
+                    </h2>
                     <p className="text-sm/relaxed">por {receita.criador}</p>
-                    <p className="text-sm/relaxed text-slate-500">{humanReadableInterval(receita.data_de_criacao)}</p>
+                    <p className="text-sm/relaxed text-slate-500">
+                      {humanReadableInterval(receita.data_de_criacao)}
+                    </p>
                   </div>
                 </CardContent>
 
                 <CardFooter>
                   <div className="flex justify-between w-full gap-2">
-
-                    <AlertDialog>
-                      <AlertDialogTrigger asChild>
-                        <Button variant='secondary'><TrashIcon className='h-4 w-4'/></Button>
-                      </AlertDialogTrigger>
-                      <AlertDialogContent className='max-w-18rem lg:max-w-[40rem]'>
-                        <AlertDialogHeader>Deseja realmente excluir a receita?</AlertDialogHeader>
-                        <AlertDialogDescription>
-                          <p>Esta ação é irreversível e excluirá permanentemente a receita do Panela Mágica.</p>
-                        </AlertDialogDescription>
-                        <AlertDialogFooter>
-                          <AlertDialogCancel>Cancelar</AlertDialogCancel>
-
-                          <AlertDialogAction
-                            disabled={isDeleting}
-                            onClick={handleExcluir(receita)}
-                          >
-                            {isDeleting ? 'Excluindo...' : 'Excluir'}
-                          </AlertDialogAction>
-                        </AlertDialogFooter>
-                      </AlertDialogContent>
-                    </AlertDialog>
-
-
+                    <Button
+                      variant="secondary"
+                      onClick={modalDelecao.openModal}
+                    >
+                      <TrashIcon className="h-4 w-4" />
+                    </Button>
                     <Link href={`/receitas/${receita.id}`}>
-                      <Button variant='outline'>Ver Receita</Button>
+                      <Button variant="outline">Ver Receita</Button>
                     </Link>
                   </div>
                 </CardFooter>
               </Card>
             ))}
           </section>
-
         </Main>
-        <Toaster/>
-        <Footer/>
+        <Toaster />
+        <Footer />
       </div>
+
+      <ModalDelecao
+        afterDelete={afterExcluir}
+        isOpen={modalDelecao.isOpen}
+        onOpenChange={modalDelecao.onOpenChange}
+      />
     </>
   );
 }
 
-export async function getServerSideProps(): Promise<GetServerSidePropsResult<HomeProps>> {
-  const receitas = await ReceitasAPI.listar()
-  return {props: {receitas}}
+export async function getServerSideProps(): Promise<
+  GetServerSidePropsResult<HomeProps>
+> {
+  const receitas = await ReceitasAPI.listar();
+  return { props: { receitas } };
 }
